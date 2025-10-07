422e57987aea07ab8949bce255e3118a8b91b2b8
lan2014-blip <lantuli14@gmail.com>
2025-10-07 17:24:29 +0200

    Add files via upload

---
+++ printingss.py
@@ -0,0 +1,18 @@
+
+# variables
+
+birthday="14 April"
+print("my bithday is",birthday)
+print(birthday,"my birthday is")
+
+# a string and a number cant be joined
+
+
+myname=input("what is your name?")
+print("hi", myname,"welcome to python class")
+
+#keywords
+
+import keyword
+
+print(keyword.kwlist)
